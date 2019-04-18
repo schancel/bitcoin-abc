62f7f74d2a927ba2b4e35b79e31f0172db3971ef
Shammah Chancellor <shammah.chancellor@gmail.com>
2019-04-17 19:12:36 -0700

    [mining] Add package accounting functions to CBlockTemplateEntry
    Summary:
    Add a private function to allow for BlockAssembler to properly
    account for package information in CBlockTemplateEntry
    
    Depends on D2863
    
    Test Plan:
      make check
    
    Reviewers: O1 Bitcoin ABC!, #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D2864

--- src/miner.h
+++ src/miner.h
@@ -50,6 +50,27 @@ struct CBlockTemplateEntry {
         : tx(_tx), txFee(_fees), txSize(_size), txSigOps(_sigOps),
           packageOrder(0), packageFee(_fees), packageSize(_size),
           packageSigOps(_sigOps) {}
+
+    // Calculate the feerate for this transaction.  Use the minimum of the
+    // package feerate, or the transaction itself.  Parents TXNs should never
+    // end up "paying for" child transactions.
+    CFeeRate FeeRate() const {
+        return int64_t(txSize) * packageFee < int64_t(packageSize) * txFee
+                   ? CFeeRate(packageFee, packageSize)
+                   : CFeeRate(txFee, txSize);
+    }
+
+private:
+    // Include a parent transactions accounting into our own.
+    // We assume that this is used in topological order by BlockAssembler.
+    void AccountForParent(CBlockTemplateEntry &parent) {
+        packageOrder = std::max(parent.packageOrder + 1, packageOrder);
+        packageFee += parent.packageFee;
+        packageSize += parent.packageSize;
+        packageSigOps += parent.packageSigOps;
+    }
+
+    friend class BlockAssembler;
 };
 
 struct CBlockTemplate {
